9156393a9ac0925df4676f6b549334e1adb2c3f8
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-08 12:14:14 -0500

    [FIX] create products work

--- src/resolvers/ProductResolver.ts
+++ src/resolvers/ProductResolver.ts
@@ -1,16 +1,27 @@
 import { Resolver, InputType, Int, Field, Mutation, Arg, Query } from "type-graphql";
 import { Product } from "../entity/Product";
 
+@InputType()
+class ProductInput {
+  @Field()
+  name!: string
+  
+  @Field(() => Int)
+  quantity!: number
+}
+
 @Resolver()
 export class ProductResolver {
 
-  @Mutation(() => Boolean)
+  @Mutation(() => Product)
   async createProduct (
-    @Arg("name") name: string,
-    @Arg("quantity", () => Int) quantity: number
+    // @Arg("name") name: string,
+    // @Arg("quantity", () => Int) quantity: number
+    @Arg("variables", () => ProductInput) variables: ProductInput
   ) {
-    await Product.insert({ name, quantity })
-    return true
+    // await Product.insert({ name, quantity })
+    const newProduct = Product.create(variables)
+    return await newProduct.save()
   }
 
   @Query(() => [Product])

f441a61d4e10659b17fa6f7f9571542de811d5be
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-08 12:40:25 -0500

    [FIX] validate dependencies

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,8 @@ export async function startServer() {
 
   const server = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [PingResolver, ProductResolver]
+      resolvers: [PingResolver, ProductResolver],
+      validate: false
     }),
     context: ({ req, res }) => ({ req, res })
   })
